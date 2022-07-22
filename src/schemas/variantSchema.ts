33a81a6cdef1e937977508999025045f0f1a059b
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-22 19:54:30 +0200

    Add validation for array of sizes to be unique

--- src/schemas/variantSchema.ts
+++ src/schemas/variantSchema.ts
@@ -1,5 +1,7 @@
 import { Schema } from 'mongoose';
 import validator from 'validator';
+import AppError from '../utils/AppError';
+import { isUniqueArray } from '../utils/helperFunctions';
 
 export interface ISize {
   size: string;
@@ -12,42 +14,58 @@ export interface IVariant {
   images: string[];
 }
 
-const sizeSchema = new Schema<ISize>({
-  size: {
-    type: String,
-    required: [true, 'Must have size'],
-    enum: ['S', 'M', 'L', 'XL', 'XXL', 'XXXl'],
+const sizeSchema = new Schema<ISize>(
+  {
+    size: {
+      type: String,
+      required: [true, 'Must have size'],
+      enum: ['S', 'M', 'L', 'XL', 'XXL', 'XXXl'],
+    },
+    count: {
+      type: Number,
+      required: true,
+      default: 1,
+      min: 0,
+      validate: [
+        (val: number) => validator.isInt(`${val}`),
+        'count must be integer',
+      ],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Must have price'],
+      min: 0,
+    },
   },
-  count: {
-    type: Number,
-    required: true,
-    default: 1,
-    min: 0,
-    validate: [
-      (val: number) => validator.isInt(`${val}`),
-      'count must be integer',
-    ],
-  },
-  price: {
-    type: Number,
-    required: [true, 'Must have price'],
-    min: 0,
-  },
-});
+  {
+    _id: false,
+  }
+);
 
-const variantSchema = new Schema<IVariant>({
-  color: {
-    type: String,
-    required: true,
-    validate: [validator.isHexColor, 'Invalid color'],
+const variantSchema = new Schema<IVariant>(
+  {
+    color: {
+      type: String,
+      required: true,
+      validate: [validator.isHexColor, 'Invalid color'],
+    },
+    sizes: {
+      type: [sizeSchema],
+      required: [true, 'Varient must have list of sizes'],
+    },
+    images: {
+      type: [String],
+    },
   },
-  sizes: {
-    type: [sizeSchema],
-    required: [true, 'Varient must have list of sizes'],
-  },
-  images: {
-    type: [String],
-  },
+  {
+    _id: false,
+  }
+);
+
+variantSchema.pre('validate', function beforeValidateVariant(next) {
+  if (!isUniqueArray(this.sizes, 'size'))
+    return next(new AppError(`Thare are duplicates of sizes`, 400));
+  next();
 });
 
 export default variantSchema;
