9ced27c48a281cd11f525f9a340d04e719b0749a
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-08-02 01:44:55 +0200

    Fix Populate User for reviews

--- src/models/reviewModel.ts
+++ src/models/reviewModel.ts
@@ -82,13 +82,13 @@ reviewSchema.post(/^findOneAnd/, async function () {
   await ReviewModel.calcAverageRatings(this.product);
 });
 
+reviewSchema.pre(/^find/, function propulateReviewsUers() {
+  this.populate({ path: 'user', select: 'name photo' });
+});
+
 const ReviewModel = mongoose.model<IReview, ReviewModel>(
   'review',
   reviewSchema
 );
-
-reviewSchema.pre(/^find/, function propulateReviewsUers() {
-  this.populate({ path: 'user', select: 'name photo' });
-});
 
 export default ReviewModel;
