beb9076ebc2daac5c6f36dbd8d99843d3788fd6f
Leeefo <a.m.abouelleef@gmail.com>
2022-08-11 14:43:07 +0200

    Adding Image Upload feature

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,4 +1,6 @@
 import ProductModel from '../models/productModel';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
+
 import {
   createModel,
   deleteModel,
@@ -7,8 +9,17 @@ import {
   updateModel,
 } from '../utils/handlerFactory';
 
+import { upload } from "../utils/uploadConfig"
+
 export const getAllProduct = getAll(ProductModel);
 export const getProduct = getOne(ProductModel);
 export const deleteProduct = deleteModel(ProductModel);
 export const addProduct = createModel(ProductModel);
 export const updateProduct = updateModel(ProductModel);
+export const uploadImages = (req: Request, res: Response, next: NextFunction) => {
+
+  res.status(201).json({
+    status: 'success',
+    data: req.files
+  });
+}
