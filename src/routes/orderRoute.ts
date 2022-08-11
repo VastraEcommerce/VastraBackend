c06cd25f3f1c137071c6965ab6a14d433a706d41
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-08-12 01:34:56 +0200

    Add order route

--- src/routes/orderRoute.ts
+++ src/routes/orderRoute.ts
@@ -1,14 +1,23 @@
 import express from 'express';
+import { protect, restrictTo } from '../controllers/authController';
 import {
   addOrder,
   deleteOrder,
   getAllOrder,
   getOrder,
+  setBodyForOrders,
   updateOrder,
 } from '../controllers/orderController';
+
 const ordersRouter = express.Router();
 
-ordersRouter.route('/').get(getAllOrder).post(addOrder);
+ordersRouter.use(protect);
+
+ordersRouter
+  .route('/')
+  .get(setBodyForOrders, getAllOrder)
+  .post(restrictTo('user'), setBodyForOrders, addOrder);
+
 ordersRouter.route('/:id').get(getOrder).patch(updateOrder).delete(deleteOrder);
 
 export default ordersRouter;
