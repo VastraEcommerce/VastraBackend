7bd7261a56b0cf5a4d8b251c294818e143b87fdc
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-08-12 19:32:00 +0200

    Add Domain name for dev branch

--- src/config/allowedOrigin.ts
+++ src/config/allowedOrigin.ts
@@ -2,6 +2,7 @@ export const allowedOrigins = [
   'https://checkout.stripe.com',
   'https://fierce-shelf-11530.herokuapp.com',
   'https://vastra-frontend.vercel.app',
+  'https://vastra-frontend-git-dev-vastra.vercel.app',
   'http://127.0.0.1:5500',
   'http://localhost:3500',
   'http://localhost:3000',
